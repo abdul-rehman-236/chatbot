5d8c3b8b611b9a6f92e9eb16870745a63a8b28a6
Hannan-git-hub <hannandon43@gmail.com>
2025-08-24 15:59:29 +0500

    Configure chakra ui and react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { Provider } from '@/components/ui/provider'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
 )
